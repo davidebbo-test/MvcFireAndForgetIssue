181d8e97264bf3b269fd19b28d8311b2654a1184
davidebbo <david.ebbo@microsoft.com>
2014-03-04 21:49:38 -0800

    Initial commit

---
+++ WebApplicationFireAndForget/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationFireAndForget.Startup))]
+namespace WebApplicationFireAndForget
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
